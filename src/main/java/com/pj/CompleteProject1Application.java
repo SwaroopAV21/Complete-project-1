baf6747b361155ac93bc2cc52e42878df886afaa
Swaroop A V <swaroopav0303@gmail.com>
2023-10-27 23:43:22 +0530

    initial commit

---
+++ src/main/java/com/pj/CompleteProject1Application.java
@@ -0,0 +1,13 @@
+package com.pj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompleteProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompleteProject1Application.class, args);
+	}
+
+}
